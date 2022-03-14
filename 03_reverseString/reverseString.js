e8ca323284706c4d1d7dbdfaabfbc4108082b859
Daniel <100245905+birchbeak@users.noreply.github.com>
2022-03-13 20:31:09 -0700

    Changed to pass unit tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let rString = '';
+    let strArray = string.split('');
+    for (i=strArray.length-1;i>=0;i--){
+        rString += strArray[i];
+    }
+    return rString;
 };
 
 // Do not edit below this line
