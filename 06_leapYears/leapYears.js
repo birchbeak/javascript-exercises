2821c6cca6e53f00e6af5cbb2f6f14ece60fef02
Daniel <100245905+birchbeak@users.noreply.github.com>
2022-03-14 00:48:52 -0700

    Set leapYears.js to comply with unit test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if(typeof year !== 'number') return 'ERROR';
 
+    if(!(year%100) && (year%400) || (year%4)) return false;
+    return true;
 };
 
 // Do not edit below this line
