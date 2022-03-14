cc429e5e23cdd976f96079ebf453080c1fff66e2
Daniel <100245905+birchbeak@users.noreply.github.com>
2022-03-13 23:13:14 -0700

    Make sumAll submit to the whims of the unit test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,16 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    if(typeof(first) !== 'number'|| typeof(second) !== 'number') return 'ERROR';
+    if (first <0 || second<0) return 'ERROR';
+    let sum=0;
+    if(first>second){
+        first= first^second;
+        second= first^second;
+        first= first^second;
+    }
+    for(; first<=second; first++){
+        sum+= first;
+    }
+    return sum;
 
 };
 
