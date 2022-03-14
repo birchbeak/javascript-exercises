cb1d2ba10e8211f9557edc69975c0c121e9d76aa
Daniel <100245905+birchbeak@users.noreply.github.com>
2022-03-14 01:06:08 -0700

    Make comply with unit test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  return round((temp-32)*5/9);
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+return round(temp*9/5+32);
 };
+function round(num){
+  return Math.ceil(num*10)/10;
+}
 
 // Do not edit below this line
 module.exports = {
