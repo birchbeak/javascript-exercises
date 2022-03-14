9f5b1a3d674d109f93bad443e13c93db2264cf3a
Daniel <100245905+birchbeak@users.noreply.github.com>
2022-03-13 22:09:06 -0700

    Conform to unit test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array){
+    let rem= null;
 
+    for(let k=1; k<arguments.length; k++){
+        rem = arguments[k];
+        for(let i=0; i<array.length; i++){
+            if(array[i]===rem) array.splice(i,1);
+        }
+    }
+    console.log(array);
+    return array;
 };
 
 // Do not edit below this line
