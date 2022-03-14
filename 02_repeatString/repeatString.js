99cb3dac811b0a108863fce24e7625d68481239c
Daniel <100245905+birchbeak@users.noreply.github.com>
2022-03-13 19:49:48 -0700

    Satisfy unit tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let strCpy=string;
+    if (num<0) return 'ERROR';
+    if (num===0) return '';
 
+    for(i=1;i<num;i++){
+        string += strCpy;
+    }
+
+    return string;
 };
 
 // Do not edit below this line
