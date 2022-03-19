14f420de9205a87144766d6b9b20b31626048c3a
Daniel <100245905+birchbeak@users.noreply.github.com>
2022-03-18 23:02:44 -0700

    Satisfy fibinnaci tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    let fib=1;
+    let pre=0;
+    if(index < 1) return 'OOPS';
+    //if(index===1 || index===2) return fib;
 
+    for(let i=1; i<index; i++){
+        fib=pre + fib;
+        pre=fib-pre;
+    }
+    console.log(fib);
+    return fib;
 };
 
 // Do not edit below this line
