061e7e1cf7a4a98db60487ca9828b3af5c3240b7
Daniel <100245905+birchbeak@users.noreply.github.com>
2022-03-18 18:30:49 -0700

    Satisfy all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if(arr.length<= 0) return 0;
+  let sum=0;
+  for(let el of arr){
+    sum+=el;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	if(arr.length<= 0) return 0;
+  let product=1;
+  for(let el of arr){
+    product*=el;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	let result= 1;
+
+  for(let i=0;i<pow;i++){
+    result*=num;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(index) {
+  let result=1;
+
+	for(let i=index; i>0; i--){
+    result*=i;
+  }
+  return result;
 };
 
 // Do not edit below this line
