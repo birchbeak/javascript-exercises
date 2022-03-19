4279bfe0e80a085f91dca55ee28f6b1aa37f0629
Daniel <100245905+birchbeak@users.noreply.github.com>
2022-03-18 22:08:49 -0700

    Satisfied all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let palArr= str.split('');
+    let compStr= '';
+    palArr= palArr.filter(ch=>{
+        return /[a-z]|[A-Z]/gi.test(ch);
+    });
+    console.log(palArr.join(''));
+    compStr= palArr.slice(Math.ceil(palArr.length/2));
+    palArr= palArr.slice(0, palArr.length/2);
+    compStr= compStr.reverse();
 
+    //console.log(compStr.join('').toLowerCase());
+    //console.log(palArr.join('').toLowerCase());
+
+    return compStr.join('').toLowerCase() === palArr.join('').toLowerCase();
 };
 
 // Do not edit below this line
